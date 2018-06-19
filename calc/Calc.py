12f56d260eca6de551fb1de0618a280a52775832
Talia Flores <floreslia871@gmail.com>
2018-06-19 16:24:28 -0400

    inicio

---
+++ calc/Calc.py
@@ -0,0 +1,2 @@
+import sympy
+print "hola mundo"
---
+++ calpy/local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'calpy',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
